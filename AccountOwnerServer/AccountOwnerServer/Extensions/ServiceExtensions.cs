6e8daec60fb1733e22bbf2cfc74b9f8b5fe910f2
b2jtheone <b2jtheone@gmail.com>
2023-05-10 18:20:21 +0200

    Initialisation du projet et ajout des fichiers de création de la BDD

---
+++ AccountOwnerServer/AccountOwnerServer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace AccountOwnerServer.Extensions
+{
+    public static class ServiceExtensions
+    {
+        /**
+         * CORS (Cross-Origin Resource Sharing) : est un mécanisme qui donne des droits à l'utilisateur pour accéder 
+         * aux ressources du serveur sur un domaine différent.
+         * Comme nous allons utiliser Angular côté client sur un domaine différent de celui du serveur, 
+         * la configuration de CORS est obligatoire.
+         */
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorspPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+            });
+        }
+    }
+}
---
+++ AccountOwnerServer/AccountOwnerServer/Program.cs
@@ -0,0 +1,41 @@
+using AccountOwnerServer.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+app.UseHttpsRedirection();
+
+/**
+ * ermet d'utiliser des fichiers statiques pour la requête. Si nous ne définissons pas de chemin pour les 
+ * fichiers statiques, le dossier wwwroot de notre explorateur de solutions sera utilisé par défaut.
+ */
+app.UseStaticFiles();
+
+// Transmettra les en-têtes du proxy à la requête en cours. Cela nous aidera lors du déploiement de Linux.
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorspPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
